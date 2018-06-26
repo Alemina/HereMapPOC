7d2c9d41e85000695d800353b4a7e4e82dbc3768
Marcin Kobylinski <marcin.kobylinski@globallogic.com>
2018-06-26 11:48:04 +0200

    print default polygon

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-﻿(function() {
+﻿var polygon = '';
+
+(function() {
   console.log("js works");
 
   var xhr = new XMLHttpRequest();
@@ -6,7 +8,7 @@
   xhr.onload = function (e) {
     if (xhr.readyState === 4) {
       if (xhr.status === 200) {
-        console.log(xhr.responseText);
+        polygon = xhr.responseText;
       } else {
         console.error(xhr.statusText);
       }
@@ -17,8 +19,56 @@
   };
   xhr.send(null);
 
-
-  
-  console.log("js end");
-  
 })()
+
+
+
+function polygonTest(map){
+  console.log('here')
+  var lineString = new H.geo.LineString(
+    [52, 13, 100, 48, 2, 100, 48, 16, 100, 52, 13, 100],
+    'values lat lng alt'
+  );
+  map.addObject(
+    new H.map.Polygon(lineString, {
+      style: {
+        fillColor: '#FFFFCC',
+        strokeColor: '#829',
+        lineWidth: 8
+      }
+    })
+  );
+  console.log('here2')
+}
+
+(function initializeMap(){
+  console.log("inside")
+  //Step 1: initialize communication with the platform
+  var platform = new H.service.Platform({
+    app_id: 'DemoAppId01082013GAL',
+    app_code: 'AJKnXv84fjrb0KIHawS0Tg',
+    useCIT: true,
+    useHTTPS: true
+  });
+  var pixelRatio = window.devicePixelRatio || 1;
+  var defaultLayers = platform.createDefaultLayers({
+    tileSize: pixelRatio === 1 ? 256 : 512,
+    ppi: pixelRatio === 1 ? undefined : 320
+  });
+
+  //Step 2: initialize a map  - not specificing a location will give a whole world view.
+  var map = new H.Map(document.getElementById('map'),
+    defaultLayers.normal.map, {pixelRatio: pixelRatio});
+
+  //Step 3: make the map interactive
+  // MapEvents enables the event system
+  // Behavior implements default interactions for pan/zoom (also on mobile touch environments)
+  var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+
+  // Create the default UI components
+  var ui = H.ui.UI.createDefault(map, defaultLayers);
+
+  // Now use the map as required...
+  setTimeout(function() {polygonTest(map) }, 2000);
+
+})();
